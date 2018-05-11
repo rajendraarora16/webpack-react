52ce2990f0bb007d8de70d446c5a3eeb741a0a3d
Rajendra Arora <rajendraarora161992@gmail.com>
2018-05-11 15:00:25 +0530

    React app with webpack, server.js implemented with scss

---
+++ src/client/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './component/App';
+
+import styles from './scss/application.scss';
+
+ReactDOM.render(<App />, document.getElementById('root'));
